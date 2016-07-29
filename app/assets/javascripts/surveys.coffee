df41e97ff7fa41ebbd9aa118fe3626da244c3a28
Armaan Butt <armaan.r.butt@gmail.com>
2016-07-28 20:30:08 -0400

    Surveys now show course info

--- app/assets/javascripts/surveys.coffee
+++ app/assets/javascripts/surveys.coffee
@@ -5,6 +5,10 @@ sendDataToServer = (survey) ->
   resultAsString = JSON.stringify(survey.data)
   alert(resultAsString)
 
-$ ->
+ready = ->
   survey = new Survey.Survey(surveyJSON, "surveyContainer")
   survey.onComplete.add(sendDataToServer)
+
+
+$(document).ready(ready)
+$(document).on('page:load', ready)
--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,40 +1,9 @@
 class SurveysController < ApplicationController
 
-  # GET /surveys
-  # GET /surveys.json
   def index
-
+    @course = Course.find_by_id(params[:course])
   end
 
-  # GET /surveys/1
-  # GET /surveys/1.json
-  def show
-  end
-
-  # GET /surveys/new
-  def new
-    @survey = Survey.new
-  end
-
-  # GET /surveys/1/edit
-  def edit
-  end
-
-  # POST /surveys
-  # POST /surveys.json
-  def create
-    @survey = Survey.new(survey_params)
-
-    respond_to do |format|
-      if @survey.save
-        format.html { redirect_to @survey, notice: 'Survey was successfully created.' }
-        format.json { render :show, status: :created, location: @survey }
-      else
-        format.html { render :new }
-        format.json { render json: @survey.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.

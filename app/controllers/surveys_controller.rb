a19c5e87cd411057f899a7663f207126ebfbc931
Dmitry Vasin <dmitry.vasin@soti.net>
2016-08-10 14:46:54 -0400

    Aggregation subtraction now works

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -5,6 +5,16 @@ class SurveysController < ApplicationController
     # this action will be called when the rout url "surveys/save" is hit and is passed the json variable
     def create
         if validate_user_for_course
+            previous_answer = Response.find_by(:user_id => current_user.id, :course_id => params[:course])
+            if previous_answer
+              json_response = eval(previous_answer.response)
+              json_response.each do |question,answer|
+                prev_aggregated_response_row=AggregatedResponse.find_by(:answer => answer,:question => question, :course_id => params[:course])
+                if prev_aggregated_response_row
+                  prev_aggregated_response_row.update_attributes!(:count => (prev_aggregated_response_row.count-1))
+                end
+              end
+            end
             Response.find_or_initialize_by(:user_id => current_user.id, :course_id => params[:course]).update_attributes!(:response => response_params)
             response_params.each do |question,answer|
               aggregated_response_row=AggregatedResponse.find_by(:answer => answer,:question => question, :course_id => params[:course])

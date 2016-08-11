788e03e6d61ce988a82afe3ce03ddead28af29a2
Dmitry Vasin <dmitry.vasin@soti.net>
2016-08-11 13:53:51 -0400

    Removed additional migration files

--- db/migrate/20160502013655_add_course_reference_to_survey_response.rb
+++
@@ -1,5 +0,0 @@
-class AddCourseReferenceToSurveyResponse < ActiveRecord::Migration
-  def change
-    add_reference :response_sets, :course, index: true, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -22,33 +22,6 @@ ActiveRecord::Schema.define(version: 20160809152259) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "answers", force: :cascade do |t|
-    t.integer  "question_id"
-    t.text     "text"
-    t.text     "short_text"
-    t.text     "help_text"
-    t.integer  "weight"
-    t.string   "response_class"
-    t.string   "reference_identifier"
-    t.string   "data_export_identifier"
-    t.string   "common_namespace"
-    t.string   "common_identifier"
-    t.integer  "display_order"
-    t.boolean  "is_exclusive"
-    t.integer  "display_length"
-    t.string   "custom_class"
-    t.string   "custom_renderer"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "default_value"
-    t.string   "api_id"
-    t.string   "display_type"
-    t.string   "input_mask"
-    t.string   "input_mask_placeholder"
-  end
-
-  add_index "answers", ["api_id"], name: "uq_answers_api_id", unique: true
-
   create_table "commontator_comments", force: :cascade do |t|
     t.string   "creator_type"
     t.integer  "creator_id"
@@ -109,31 +82,6 @@ ActiveRecord::Schema.define(version: 20160809152259) do
 
   add_index "courses_users", ["course_id", "user_id"], name: "index_courses_users_on_course_id_and_user_id"
 
-  create_table "dependencies", force: :cascade do |t|
-    t.integer  "question_id"
-    t.integer  "question_group_id"
-    t.string   "rule"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "dependency_conditions", force: :cascade do |t|
-    t.integer  "dependency_id"
-    t.string   "rule_key"
-    t.integer  "question_id"
-    t.string   "operator"
-    t.integer  "answer_id"
-    t.datetime "datetime_value"
-    t.integer  "integer_value"
-    t.float    "float_value"
-    t.string   "unit"
-    t.text     "text_value"
-    t.string   "string_value"
-    t.string   "response_other"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
@@ -146,48 +94,6 @@ ActiveRecord::Schema.define(version: 20160809152259) do
   add_index "posts", ["course_id"], name: "index_posts_on_course_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
-  create_table "question_groups", force: :cascade do |t|
-    t.text     "text"
-    t.text     "help_text"
-    t.string   "reference_identifier"
-    t.string   "data_export_identifier"
-    t.string   "common_namespace"
-    t.string   "common_identifier"
-    t.string   "display_type"
-    t.string   "custom_class"
-    t.string   "custom_renderer"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "api_id"
-  end
-
-  add_index "question_groups", ["api_id"], name: "uq_question_groups_api_id", unique: true
-
-  create_table "questions", force: :cascade do |t|
-    t.integer  "survey_section_id"
-    t.integer  "question_group_id"
-    t.text     "text"
-    t.text     "short_text"
-    t.text     "help_text"
-    t.string   "pick"
-    t.string   "reference_identifier"
-    t.string   "data_export_identifier"
-    t.string   "common_namespace"
-    t.string   "common_identifier"
-    t.integer  "display_order"
-    t.string   "display_type"
-    t.boolean  "is_mandatory"
-    t.integer  "display_width"
-    t.string   "custom_class"
-    t.string   "custom_renderer"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "correct_answer_id"
-    t.string   "api_id"
-  end
-
-  add_index "questions", ["api_id"], name: "uq_questions_api_id", unique: true
-
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
     t.integer  "followed_id"
@@ -198,22 +104,6 @@ ActiveRecord::Schema.define(version: 20160809152259) do
   add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
   add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
   add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
-
-  create_table "response_sets", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "survey_id"
-    t.string   "access_code"
-    t.datetime "started_at"
-    t.datetime "completed_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "api_id"
-    t.integer  "course_id"
-  end
-
-  add_index "response_sets", ["access_code"], name: "response_sets_ac_idx", unique: true
-  add_index "response_sets", ["api_id"], name: "uq_response_sets_api_id", unique: true
-  add_index "response_sets", ["course_id"], name: "index_response_sets_on_course_id"
 
   create_table "responses", force: :cascade do |t|
     t.text     "response"
@@ -261,58 +151,14 @@ ActiveRecord::Schema.define(version: 20160809152259) do
     t.string   "qeustion35"
     t.string   "question36"
     t.string   "question37"
-    t.integer  "user_id_id"
-    t.integer  "course_id_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.integer  "user_id"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "submissions", ["course_id_id"], name: "index_submissions_on_course_id_id"
-  add_index "submissions", ["user_id_id"], name: "index_submissions_on_user_id_id"
-
-  create_table "survey_sections", force: :cascade do |t|
-    t.integer  "survey_id"
-    t.string   "title"
-    t.text     "description"
-    t.string   "reference_identifier"
-    t.string   "data_export_identifier"
-    t.string   "common_namespace"
-    t.string   "common_identifier"
-    t.integer  "display_order"
-    t.string   "custom_class"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "survey_translations", force: :cascade do |t|
-    t.integer  "survey_id"
-    t.string   "locale"
-    t.text     "translation"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "surveys", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.string   "access_code"
-    t.string   "reference_identifier"
-    t.string   "data_export_identifier"
-    t.string   "common_namespace"
-    t.string   "common_identifier"
-    t.datetime "active_at"
-    t.datetime "inactive_at"
-    t.string   "css_url"
-    t.string   "custom_class"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "display_order"
-    t.string   "api_id"
-    t.integer  "survey_version",         default: 0
-  end
-
-  add_index "surveys", ["access_code", "survey_version"], name: "surveys_access_code_version_idx", unique: true
-  add_index "surveys", ["api_id"], name: "uq_surveys_api_id", unique: true
+  add_index "submissions", ["course_id"], name: "index_submissions_on_course_id"
+  add_index "submissions", ["user_id"], name: "index_submissions_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -331,31 +177,5 @@ ActiveRecord::Schema.define(version: 20160809152259) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-  create_table "validation_conditions", force: :cascade do |t|
-    t.integer  "validation_id"
-    t.string   "rule_key"
-    t.string   "operator"
-    t.integer  "question_id"
-    t.integer  "answer_id"
-    t.datetime "datetime_value"
-    t.integer  "integer_value"
-    t.float    "float_value"
-    t.string   "unit"
-    t.text     "text_value"
-    t.string   "string_value"
-    t.string   "response_other"
-    t.string   "regexp"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "validations", force: :cascade do |t|
-    t.integer  "answer_id"
-    t.string   "rule"
-    t.string   "message"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end

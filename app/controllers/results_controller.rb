47adc105bf7fb972a07ae7ba1116f103f9f06103
Armaan Butt <armaan.r.butt@gmail.com>
2016-08-23 21:59:00 -0400

    More UI changes!!!!!!!!!!!!

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,7 +1,10 @@
 class ResultsController < ApplicationController
   layout "application"
   def show
-    @results = AggregatedResponse.where(:course_id => params[:course])
+    # @questions = Question.all
+    # @answers = AggregatedResponse.where(course_id: params[:course])
+    # cleaned = Hash.new
+    @course = Course.find_by(id: params[:course])
   end
 
 
